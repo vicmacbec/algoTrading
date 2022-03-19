159199b96ae07f10dea839a0cf13946bfadcbafb
Víctor Macedo Becerril <vicmacbec1@hotmail.com>
2022-03-19 11:42:16 -0600

    Creating template file for strategies

---
+++ Scripts/template.R
@@ -0,0 +1,37 @@
+#### Script information ####
+
+
+#### Load libraries ####
+library(binancer)
+library(ggplot2)
+library(scales)
+library(data.table)
+library(TTR)
+library(stringr)
+library(zoo)
+library(plotly)
+library(purrr)
+library(roll)
+library(patchwork)
+
+
+#### Functions ####
+
+
+#### Load data ####
+
+
+#### Initial parameters ####
+
+
+#### Strategy ####
+
+
+#### Plots ####
+
+
+#### Saving data ####
+
+
+#### Tests ####
+
