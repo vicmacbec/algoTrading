c1c3b7db9eb4698ee8f106ca2af70cedfc06c60a
Víctor Macedo Becerril <vicmacbec1@hotmail.com>
2022-05-05 20:50:30 -0500

    #7 Making usable the bash file

--- Scripts/Strategies/MASlope_ATRStopL_Prod.sh
+++ Scripts/Strategies/MASlope_ATRStopL_Prod.sh
@@ -1,7 +1,15 @@
 # !/bin/bash 
 
+# sh ~/Drive/Codigos/AlgoTrading/Scripts/Strategies/MASlope_ATRStopL_Prod.sh >> ~/Drive/Codigos/AlgoTrading/DataOut/MASlope_ATRStopLoss/Logs/$(date '+%Y_%m_%d')_Trades.log
+
+echo 'Start Strategy'
 echo 'Date:   ' $(date '+%Y-%m-%d %H:%M:%S')
+echo ''
 
 symbol='FXSBUSD'
 echo 'Symbol: ' $symbol
-Rscript /home/victor/Drive/Codigos/AlgoTrading/Scripts/Strategies/MASlope_ATRStopL_Prod.R $symbol >> Drive/Codigos/AlgoTrading/DataOut/MASlope_ATRStopLoss/allLogs/$(date '+%Y_%m_%d')_$symbol
+Rscript ~/Drive/Codigos/AlgoTrading/Scripts/Strategies/MASlope_ATRStopL_Prod.R $symbol >> ~/Drive/Codigos/AlgoTrading/DataOut/MASlope_ATRStopLoss/allLogs/$(date '+%Y_%m_%d_%H:%M:%S')_$symbol.log
+
+echo 'End Strategy'
+echo ''
+echo ''
