5c96295a0291b4e8b6f766b38d51adb33f7c4ec4
Víctor Macedo Becerril <vicmacbec1@hotmail.com>
2022-11-10 19:21:55 -0600

    #9 Model improvements: - Automatically getting data for 4 hours or 1 day - Change target by 1.02 times - Automaically get features - Change the split data function - Scale data - Comparing cumulative distribution functions - Ploting train/test curves

--- Scripts/Strategies/ml_tradingRules.R
+++ Scripts/Strategies/ml_tradingRules.R
@@ -17,7 +17,8 @@
 print("#### Load libraries ####")
 suppressMessages(library(binancer))
 # suppressMessages(library(keyring))
-# suppressMessages(library(ggplot2))
+suppressMessages(library(ggplot2))
+suppressMessages(library(plotly))
 # suppressMessages(library(scales))
 suppressMessages(library(data.table))
 suppressMessages(library(TTR))
@@ -57,8 +58,6 @@ print("#### Load data ####")
 # binance_coins()
 # binance_coins_prices() #%>% View
 # binance_symbols() %>% grep(pattern = "^BUSD|*BUSD", value = TRUE) -> BUSDpairs
-allOrders <- fread(paste0(pathDataInAllOrders, "allOrders_year_20220421.csv"))
-allData <- fread(paste0(pathDataInAllData, "allData_year_4h_20220421.csv"))
 
 
 #### Initial parameters ####
@@ -82,29 +81,33 @@ fee <- 0.00075
 
 args <- commandArgs(trailingOnly=TRUE)
 if(length(args) == 0){
-  pair <- "FXSBUSD"
+  pair <- "BTCBUSD"
 }else{
   pair <- args[1]
 }
 
 print(paste0("Pair: ", pair))
 
-klines <- allData[symbol == pair, .(symbol, open_time, open, high, low, close, volume, close_time, trades)]
+# # Cada 4 horas
+# days <- 2000
+# date <-  as.Date("2022-11-08") # Sys.Date() # Last day
+# klines <- NULL
+# for(day in days:0){
+#   tmp <- binance_klines(pair, limit = 24/4, interval = '4h', start_time = date - day)
+#   klines <- rbind(klines, tmp) %>% unique()
+# }
+# klines
 
-difftime(floor_date(Sys.time() + 4 * 3600, unit = "hour"), Sys.time(), units = "secs") #%>% Sys.sleep()
-
-# klines <- binance_klines(pair, limit = limit, interval = '5m') # 'BTCUSDT'
-# klines <- binance_klines(pair, limit = limit, interval = '5m', start_time = '2021-12-03')
-tmp <- binance_klines(pair, limit = 400, interval = '4h'
-                      )[, .(symbol, open_time, open, high, low, close, volume, close_time, trades)]
-# Do not add 5 hours because rbind bind the POSIXct columns in the same zone 
-# tmp[, ':='(open_time = open_time + 5*60*60, close_time = close_time + 5*60*60)] # Add 7 hours to match with TrendingView
-
-tmp[.N, open_time]
-klines[.N, open_time]
-rbind(klines, tmp)[.N, open_time]
-klines <- rbind(klines, tmp)[order(open_time, -trades)] %>% distinct(open_time, .keep_all = TRUE)
-# klines[order(open_time)]
+# Diario
+years <- 10
+date <-  as.Date("2022-11-08") # Sys.Date() # Last day
+klines <- NULL
+for(year in years:0){
+  tmp <- binance_klines(pair, limit = 370, interval = '1d', start_time = date - year * 370
+  )[, .(symbol, open_time, open, high, low, close, volume, close_time, trades)]
+  klines <- rbind(klines, tmp) %>% unique()
+}
+klines
 
 
 print(paste0("Number of registers from ", pair, " pair: ", klines %>% nrow()))
@@ -170,9 +173,11 @@ klines2[, shortStopLoss := close + atr * multiplier]
 klines2[, longStopLoss := close - atr * multiplier]
 
 # Target #
+# Quizás cambiar target 1 si es que es más grande por 10 % de su valor inicial anterior
+# Quizás solo quedarnos con los registros que están en cierta cantidad de precio
 klines2[, close_lead := shift(close, type = "lead")]
-klines2[, target := ifelse(close_lead > close, "1", "0")]
-
+klines2[, target := ifelse(close_lead > (1.02 * close), "1", "0")]
+table(klines2[, target])
 
 #### Features relations ####
 klines2[, 
@@ -184,7 +189,21 @@ klines2[,
         ] %>% na.omit() -> klines3
 
 
-ggpairs(klines3, columns = 4:26, 
+#### Feauter selection ####
+# All features
+features1 <- c("open", "high", "low", "close", "volume", "trades", 
+               "ohlc4", "ema", "atr", "maSlope", 
+               "rollmean05", "rollmean10", "rollmean20", "rollmean51",
+               "dn", "up", "pctB", "macd", "signal", "RSI", "shortStopLoss", "longStopLoss")
+# Half most important features
+features2 <- c("open", "volume", "trades", 
+               "atr", "maSlope", 
+               # "dn", "up", 
+               "pctB", "macd", "signal", "RSI")
+features <- features2
+
+
+ggpairs(klines3, columns = features, 
         aes(color = target, alpha = 0.5)
         # lower = list(continuous = "smooth")
         )
@@ -193,45 +212,72 @@ ggpairs(klines3, columns = 4:26,
 #### XGBoost ####
 
 # Split train test data
-sample <- sample(c(TRUE,FALSE), nrow(klines3), replace=TRUE, prob=c(0.7,0.3))
+# sample <- sample(c(TRUE,FALSE), nrow(klines3), replace=TRUE, prob=c(0.7,0.3))
+sample <- createDataPartition(klines3[, target], p = 0.7, list = FALSE)
 
-x_train <- klines3[sample, 
-                   .(open, high, low, close, volume, trades, 
-                     ohlc4, ema, atr, maSlope, rollmean05, rollmean10, rollmean20, rollmean51,
-                     dn, up, pctB, macd, signal, RSI, shortStopLoss, longStopLoss)]
+x_train <- klines3[sample, ..features]
 y_train <- klines3[sample, target]
-x_test <- klines3[!sample, 
-                  .(open, high, low, close, volume, trades, 
-                    ohlc4, ema, atr, maSlope, rollmean05, rollmean10, rollmean20, rollmean51,
-                    dn, up, pctB, macd, signal, RSI, shortStopLoss, longStopLoss)]
-y_test <- klines3[!sample, target]
+x_test <- klines3[-sample, ..features]
+y_test <- klines3[-sample, target]
 
 y_train %>% table()
 y_test %>% table()
 
+
+# Scaling data
+# To get the column names into the lapply .SD function
+# https://stackoverflow.com/questions/59074426/using-sd-column-names-in-lapply-with-data-tables
+x_test[, c(features) := lapply(seq_along(names(.SD)), 
+                               function(y, n, i){ (y[[i]] - mean(unlist(x_train[, n[[i]], with = FALSE]))) / sd(unlist(x_train[, n[[i]], with = FALSE])) },
+                               y = .SD,
+                               n = names(.SD)), 
+       .SDcols = features]
+x_train[, c(features) := lapply(.SD, function(x){scale(x = x)}), .SDcols = features]
+
+
 xgboost_train <- xgb.DMatrix(data = data.matrix(x_train), label = y_train)
 xgboost_test <- xgb.DMatrix(data = data.matrix(x_test), label = y_test)
 
+
+# Comparing train and test distributions
+features
+feature <- features[2]
+plot(ecdf(x_train[, ..feature][[1]]), main = feature)
+lines(ecdf(x_test[, ..feature][[1]]), col = 'blue')
+
+
 # Model
-xgboost::xgb.cv()
+# xgboost::xgb.cv()
 
 watchlist <- list(train = xgboost_train, test = xgboost_test)
 model <- xgb.train(data = xgboost_train,
                    objective = "binary:logistic",
-                   max.depth = 3,
-                   eta = 1,
-                   nthread = 2,
-                   nrounds = 1000,
-                   early_stopping_rounds = 10,
+                   # nthread = 2, # threads of the computer
+                   max.depth = 5,
+                   eta = 0.001, # learning rate (gradient, [0,1]) https://machinelearningmastery.com/tune-learning-rate-for-gradient-boosting-with-xgboost-in-python/
+                   nrounds = 10000,
+                   # early_stopping_rounds = 10,
+                   lambda = 10,
                    watchlist = watchlist
+                   # tree_method = "gpu_hist"
                    )
-summary(model)
+
+curves <- rbind(model$evaluation_log[, .(iter, type = "train", values = train_error)],
+                model$evaluation_log[, .(iter, type = "test", values = test_error)])
+p <- ggplot(curves, aes(x = iter, y = values, color = type)) +
+  geom_point() +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  ggtitle("Features: All")
+  # ggtitle("Features: Using feature importance")
+p %>% ggplotly()
 
 
 xgboost::xgb.importance(model = model)
 xgboost::xgb.plot.importance(xgb.importance(model = model))
 
-# Prdeictions
+
+# Predictions
 pred_test <- predict(model, xgboost_test)
 pred_test
 
